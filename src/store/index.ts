6fa3a6a217d87527b2589271c242f9bf9a4ebc2a
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2022-03-18 01:10:32 -0300

    configure redux saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,24 @@
-import { createStore } from "redux";
-import { CartStateProps } from "./modules/cart/types/products";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from "./modules/rootSaga";
 import rootReducer from "./modules/rootReducer";
+import { CartStateProps } from "./modules/cart/types/products";
 
 export interface StateProps {
   cart: CartStateProps;
 }
 
-const store = createStore(rootReducer); // estado global iniciado []
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares =[sagaMiddleware];
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middlewares)
+)); // estado global iniciado []
+
+ sagaMiddleware.run(rootSaga);
 
 export default store;
